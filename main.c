fb46373304259f1c5a83a380e6888e652439d997
vanshikagupta0619-sketch <vanshikagupta0619@gmail.com>
2026-06-05 09:47:36 +0530

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+#define WIDTH 20
+#define HEIGHT 10
+
+char picture[HEIGHT][WIDTH];
+
+void clearPicture()
+{
+    for(int i = 0; i < HEIGHT; i++)
+    {
+        for(int j = 0; j < WIDTH; j++)
+        {
+            picture[i][j] = '_';
+        }
+    }
+}
+
+void displayPicture()
+{
+    for(int i = 0; i < HEIGHT; i++)
+    {
+        for(int j = 0; j < WIDTH; j++)
+        {
+            printf("%c", picture[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    clearPicture();
+    displayPicture();
+
+    return 0;
+}
