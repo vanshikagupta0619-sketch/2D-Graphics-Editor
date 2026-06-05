3f0b2b778eef7d45022271d7f1325a5124a1fadc
vanshikagupta0619-sketch <vanshikagupta0619@gmail.com>
2026-06-05 19:31:35 +0530

    main.c

--- main.c
+++ main.c
@@ -1,37 +1,43 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
-#define WIDTH 20
-#define HEIGHT 10
+#define WIDTH 80
+#define HEIGHT 24
+
+#define EMPTY'_'
+#define PIXEL '*'
 
 char picture[HEIGHT][WIDTH];
 
 void clearPicture()
 {
-    for(int i = 0; i < HEIGHT; i++)
-    {
-        for(int j = 0; j < WIDTH; j++)
-        {
-            picture[i][j] = '_';
-        }
-    }
-}
+    int i,j;
 
+for(i=0;i<HEIGHT;i++)
+{
+for(j=0;i<HEIGHT;i++)
+{
+picture[i][j]=EMPTY;
+}
+}
+}
 void displayPicture()
 {
-    for(int i = 0; i < HEIGHT; i++)
-    {
-        for(int j = 0; j < WIDTH; j++)
-        {
-            printf("%c", picture[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int main()
+    int i,j;
+for(i=0;i<HEIGHT;i++)
 {
-    clearPicture();
-    displayPicture();
-
-    return 0;
+for(j=0;j<WIDTH;j++)
+{
+printf("%c",picture[i][j]);
+}
+printf("\n");
+}
+}
+void setPixel(int x,int y)
+{
+    if(x>=0&&x<WIDTH&&y>=0&&y<HEIGHT)
+{
+picture[y][x]=PIXEL;
+}
 }
