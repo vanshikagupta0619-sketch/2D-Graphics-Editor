2cc8348b957b4bb0b43f35c5984c18a12294b927
vanshikagupta0619-sketch <vanshikagupta0619@gmail.com>
2026-06-07 21:47:14 +0530

    main.c

--- main.c
+++ main.c
@@ -41,3 +41,33 @@ void setPixel(int x,int y)
 picture[y][x]=PIXEL;
 }
 }
+void drawLine(int x1,int y1,int x2,int y2)
+{
+    int dx=abs(x2-x1);
+int dy=abs(y2-y1);
+int sx=(x1<x2)?1:-1;
+int sy=(y1<y2)?1:-1;
+int err=dx-dy;
+while(1)
+{
+setPixel(x1,y1);
+if(x1==x2&&y1==y2)
+{
+break;
+}
+int e2=2*err;
+if(e2>-dy)
+{
+err-=dy;
+x1+=sy;
+}
+}
+}
+void drawRectangle(int x1,int y1,int x2,int y2)
+{
+    drawLine(x1,y1,x2,y1);
+drawLine(x2,y1,x2,y2);
+drawLine(x2,y2,x1,y2);
+drawLine(x1,y2,x1,y1);
+}
+    
